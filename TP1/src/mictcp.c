96e35f3a4d6948f5413fb8f7cb73974825de8082
mtimani <timani@etud.insa-toulouse.fr>
2019-03-15 17:15:11 +0100

    TP1

--- TP1/src/mictcp.c
+++ TP1/src/mictcp.c
@@ -1,9 +1,12 @@
 #include <mictcp.h>
 #include <api/mictcp_core.h>
 
+#define D 20
+
 /*Variables globales*/
-mic_tcp_sock socket;
+mic_tcp_sock mysocket;
 mic_tcp_sock_addr addr_sock_dest;
+int next_fd = 0;
 
 /*
  * Permet de créer un socket entre l’application et MIC-TCP
@@ -14,15 +17,16 @@ int mic_tcp_socket(start_mode sm)
    int result = -1;
    printf("[MIC-TCP] Appel de la fonction: ");  printf(__FUNCTION__); printf("\n");
 
-   if((result = initialize_components(sm) == -1){
+   if((result = initialize_components(sm)) == -1){
        return -1;
    }
    else{
-        my
+        mysocket.fd = next_fd;
+        next_fd++;
+        mysocket.state = IDLE;
+        set_loss_rate(D);
+        return mysocket.fd;
    }
-   set_loss_rate(0);
-
-   return result;
 }
 
 /*
@@ -32,7 +36,13 @@ int mic_tcp_socket(start_mode sm)
 int mic_tcp_bind(int socket, mic_tcp_sock_addr addr)
 {
    printf("[MIC-TCP] Appel de la fonction: ");  printf(__FUNCTION__); printf("\n");
-   return -1;
+   if (mysocket.fd == socket){
+       memcpy((char *)&mysocket.addr,(char *)&addr, sizeof(mic_tcp_sock_addr));
+       return 0;
+   }
+   else {
+       return -1;
+   }
 }
 
 /*
@@ -42,7 +52,14 @@ int mic_tcp_bind(int socket, mic_tcp_sock_addr addr)
 int mic_tcp_accept(int socket, mic_tcp_sock_addr* addr)
 {
     printf("[MIC-TCP] Appel de la fonction: ");  printf(__FUNCTION__); printf("\n");
-    return -1;
+
+    if((mysocket.fd == socket)&&(mysocket.state != CLOSED)){
+        mysocket.state = ESTABLISHED;
+        return 0;
+    }
+    else{
+        return -1;
+    }
 }
 
 /*
@@ -52,7 +69,14 @@ int mic_tcp_accept(int socket, mic_tcp_sock_addr* addr)
 int mic_tcp_connect(int socket, mic_tcp_sock_addr addr)
 {
     printf("[MIC-TCP] Appel de la fonction: ");  printf(__FUNCTION__); printf("\n");
-    return -1;
+    if((mysocket.fd == socket)&&(mysocket.state != CLOSED)){
+        mysocket.state = ESTABLISHED;
+        addr_sock_dest = addr;
+        return 0;
+    }
+    else{
+        return -1;
+    }
 }
 
 /*
@@ -62,7 +86,31 @@ int mic_tcp_connect(int socket, mic_tcp_sock_addr addr)
 int mic_tcp_send (int mic_sock, char* mesg, int mesg_size)
 {
     printf("[MIC-TCP] Appel de la fonction: "); printf(__FUNCTION__); printf("\n");
-    return -1;
+    int size_PDU;
+    mic_tcp_pdu sent_PDU;
+
+    if ((mysocket.fd == mic_sock)&&(mysocket.state == ESTABLISHED)){
+
+        //Construction du PDU
+            //Header
+        sent_PDU.header.source_port = mysocket.addr.port;
+        sent_PDU.header.dest_port = addr_sock_dest.port;
+        sent_PDU.header.syn = 0;
+        sent_PDU.header.ack = 0;
+        sent_PDU.header.fin = 0;
+
+            //Payload
+        sent_PDU.payload.data = mesg;
+        sent_PDU.payload.size = mesg_size;
+
+        //Envoi du PDU
+        size_PDU = IP_send(sent_PDU,addr_sock_dest);
+
+        return size_PDU;
+    }
+    else{
+        return -1;
+    }
 }
 
 /*
@@ -74,7 +122,25 @@ int mic_tcp_send (int mic_sock, char* mesg, int mesg_size)
 int mic_tcp_recv (int socket, char* mesg, int max_mesg_size)
 {
     printf("[MIC-TCP] Appel de la fonction: "); printf(__FUNCTION__); printf("\n");
-    return -1;
+
+    int nb_read_bytes;
+    mic_tcp_payload PDU;
+    PDU.data = mesg;
+    PDU.size = max_mesg_size;
+
+    if ((mysocket.fd == socket)&&(mysocket.state == ESTABLISHED)){
+        //Awaiting for a PDU
+        mysocket.state = IDLE;
+
+        //PDU retrieval from the reception buffer
+        nb_read_bytes = app_buffer_get(PDU);
+
+        mysocket.state = ESTABLISHED;
+        return nb_read_bytes;
+    }
+    else{
+        return -1;
+    }
 }
 
 /*
@@ -85,7 +151,14 @@ int mic_tcp_recv (int socket, char* mesg, int max_mesg_size)
 int mic_tcp_close (int socket)
 {
     printf("[MIC-TCP] Appel de la fonction :  "); printf(__FUNCTION__); printf("\n");
-    return -1;
+
+    if ((mysocket.fd == socket)&&(mysocket.state == ESTABLISHED)){
+        mysocket.state = CLOSED;
+        return 0;
+    }
+    else {
+        return -1;
+    }
 }
 
 /*
@@ -97,4 +170,7 @@ int mic_tcp_close (int socket)
 void process_received_PDU(mic_tcp_pdu pdu, mic_tcp_sock_addr addr)
 {
     printf("[MIC-TCP] Appel de la fonction: "); printf(__FUNCTION__); printf("\n");
+
+    app_buffer_put(pdu.payload);
+    mysocket.state = ESTABLISHED;
 }
